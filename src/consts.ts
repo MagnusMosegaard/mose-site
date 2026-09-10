1e853b3aa2d32a4df5e4c1457bc3ae66d863253d
Magnus Mosegaard <mg.mosegaard@gmail.com>
2026-09-10 11:46:05 +0200

    Set site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Magnus Mosegaard';
-export const SITE_DESCRIPTION = 'Welcome to Mose's Cosmos!';
+export const SITE_DESCRIPTION = "Welcome to Mose's Cosmos!";
